f3f5d38528791183f73ad44f5dc5ba8051087080
maxwell1987 <maxwell1987@foxmail.com>
2018-06-06 18:28:38 +0800

    增加：补充遗漏的文件。

---
+++ init.bat
@@ -0,0 +1,7 @@
+@echo off
+
+copy /-y resources\devkit-java.yaml devkit-java.yaml
+copy /-y resources\after.sh after.sh
+copy /-y resources\aliases aliases
+
+echo Devkit-java initialized!
---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cp -i resources/devkit-java.yaml devkit-java.yaml
+cp -i resources/after.sh after.sh
+cp -i resources/aliases aliases
+
+echo "Devkit-java initialized!"
