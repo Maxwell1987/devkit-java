22d0d45fa4fa63764236fa61e871cf2fb0b0ffb2
maxwell1987 <maxwell1987@foxmail.com>
2018-10-12 12:04:57 +0800

    增加 mysql 的时区设置。

--- scripts/install-mysql.sh
+++ scripts/install-mysql.sh
@@ -41,6 +41,7 @@ apt-get install -y mysql-server
 # Configure MySQL Remote Access
 
 sed -i '/^bind-address/s/bind-address.*=.*/bind-address = 0.0.0.0/' /etc/mysql/my.cnf
+echo -e "[mysqld]\ndefault-time-zone='+08:00'" | tee /etc/mysql/conf.d/timezone.cnf
 
 mysql --user="root" --password="secret" -e "GRANT ALL ON *.* TO root@'0.0.0.0' IDENTIFIED BY 'secret' WITH GRANT OPTION;"
 service mysql restart
